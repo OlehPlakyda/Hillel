d6cb334b179dc5db99fcd98828f3eb31293f1fc6
Oleh Plakyda <gamermrgelo@gmail.com>
2022-09-09 20:48:53 +0300

    Completed task

--- lesson_03_hw_01/01.py
+++ lesson_03_hw_01/01.py
@@ -1,4 +1,7 @@
 # a) with the help of c
+
+print('First variant:')
+
 a = 3
 b = 8
 
@@ -7,5 +10,33 @@ print('At the start of the program:', '\na =', a, '\nb =', b)
 c = a
 a = b
 b = c
+
+print('At the end of the program:', '\na =', a, '\nb =', b)
+
+# b) without help
+
+print('\nSecond variant:')
+
+a = 'space'
+b = 'star'
+
+print('At the start of the program:', '\na =', a, '\nb =', b)
+
+a, b = b, a
+
+print('At the end of the program:', '\na =', a, '\nb =', b)
+
+# c) with the help of '+' and '-'
+
+print('\nThird variant:')
+
+a = 3
+b = 8
+
+print('At the start of the program:', '\na =', a, '\nb =', b)
+
+a += b
+b = a - b
+a -= b
 
 print('At the end of the program:', '\na =', a, '\nb =', b)
