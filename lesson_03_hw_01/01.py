c1dd296cd53172eb13f98c791f4d4cb9cb11d912
Oleh Plakyda <gamermrgelo@gmail.com>
2022-09-09 20:30:17 +0300

    Completed task

--- lesson_03_hw_01/01.py
+++ lesson_03_hw_01/01.py
@@ -1,1 +1,11 @@
-# The homework
+# a) with the help of c
+a = 3
+b = 8
+
+print('At the start of the program:', '\na =', a, '\nb =', b)
+
+c = a
+a = b
+b = c
+
+print('At the end of the program:', '\na =', a, '\nb =', b)
