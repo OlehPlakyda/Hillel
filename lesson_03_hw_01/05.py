3fb330d3f03a5cb1cb27bc56ec3345934f13200b
Oleh Plakyda <gamermrgelo@gmail.com>
2022-09-10 16:57:02 +0300

    Completed task

---
+++ lesson_03_hw_01/05.py
@@ -0,0 +1,17 @@
+a = input('Enter a three-digit number: ')
+b = int(a)
+
+# a) Using strings
+
+a = [*a]
+
+a = int(a[0]) + int(a[1]) + int(a[2])
+
+print('Exit number in variant a:', a)
+
+# b) Using number
+
+b1 = b//10
+b = b%10 + b1%10 + b1//10
+
+print('Exit number in variant b:', b)
