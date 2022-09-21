ddded74eb16f9ea44f1f6372bc00a0197e2cb58e
Oleh Plakyda <gamermrgelo@gmail.com>
2022-09-21 12:25:17 +0300

    Completed task

---
+++ lesson_04_hw_01/04.py
@@ -0,0 +1,3 @@
+num = int(input('Please enter an integer number: '))
+print('Next number for number', num, 'is', str(num + 1) + '.',
+      '\nPrevious number for number', num, 'is', str(num - 1) + '.')
