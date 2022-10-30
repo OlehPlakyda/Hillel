6d55118c3b9b81256d988057be06c3ed5bd9e76f
Oleh Plakyda <gamermrgelo@gmail.com>
2022-10-30 10:23:24 +0200

    Completed task

--- lesson_05_hw_01/03.py
+++ lesson_05_hw_01/03.py
@@ -10,7 +10,7 @@ def circles_intersect(x1, y1, r1, x2, y2, r2):  # returns boolean value
 def main():
     x1, y1, r1 = map(int, input('Enter x1 y1 r1 with spaces: ').split())
     x2, y2, r2 = map(int, input('Enter x2 y2 r2 with spaces: ').split())
-    if circles_intersect(x1, y1, r1, x2, y2, r2) is True:
+    if circles_intersect(x1, y1, r1, x2, y2, r2):
         print('These circles intersect')
     else:
         print('These circles don`t intersect')
