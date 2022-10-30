04f93b7df7e2c743b3a6401a408dcacc52ef24a7
Oleh Plakyda <gamermrgelo@gmail.com>
2022-10-30 10:37:35 +0200

    Completed task

--- lesson_05_hw_01/06.py
+++ lesson_05_hw_01/06.py
@@ -1,16 +1,15 @@
-def fibonacci_by_number(number, a):
+def fibonacci_by_number(number, a=[0, 1]):
     n = len(a)
     a.append(a[n-1] + a[n-2])
     if n+1 == number:
-        print(a[n])
-        return
+        return a
     return fibonacci_by_number(number, a)
 
 
 def main():
     number = int(input('Enter a number: '))
-    a = [0, 1]
-    fibonacci_by_number(number, a)
+    a = fibonacci_by_number(number)
+    print(a[-1])
 
 
 main()
