1509b80c3169ae660a0566fb671b0dc85d53e93a
Oleh Plakyda <gamermrgelo@gmail.com>
2022-11-05 12:28:29 +0200

    Completed task

---
+++ lesson_06_hw_01/05.py
@@ -0,0 +1,18 @@
+def calculate_wheat_chess_position(kilograms):
+    n = 0
+    for n in range(65):
+        weight = 1 * 2**(n-1) * 0.000035
+        if weight == kilograms:
+            break
+    for i in 'abcdefgh':
+        if n <= 8:
+            return f'{i}{n}'
+        n -= 8
+
+
+def main():
+    kilograms = float(input('Enter the weight of the grain in kilograms: '))
+    print('Your position in chess:', calculate_wheat_chess_position(kilograms))
+
+
+main()
