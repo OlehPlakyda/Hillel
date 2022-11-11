0c6f17711ebbf60a6b3c7e80c4d326454c8a4997
Oleh Plakyda <gamermrgelo@gmail.com>
2022-11-12 00:17:52 +0200

    Completed task

--- lesson_08_hw_01/05.py
+++ lesson_08_hw_01/05.py
@@ -1,7 +1,15 @@
 def gen_primes():
-    lst = []
-    for i in range(1, 101):
-        lst.append(i)
+    lst = [2]
+    rule = 0
+    for i in range(3, 101):
+        for j in lst:
+            if i % j != 0:
+                rule = True
+            else:
+                rule = False
+                break
+        if rule is True:
+            lst.append(i)
     return lst
 
 
